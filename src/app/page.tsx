19d761fb2c31e61bea70e67a4de900125d83eff9
Felipe Moura <felipenascimento.moura41@gmail.com>
2025-02-19 13:53:35 -0300

    feat: create first configs and code pattern

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="bg-red-500">
+      <h1>Hello World</h1>
+      <button className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">
+        Click me
+      </button>
+    </div>
+  );
+}
